195def5857f45f468d4d029fb45e59f7d67e1185
hany606 <h.hamed.elanwar@gmail.com>
2018-05-27 12:51:07 +0200

    enhance code more & finish the scatter graph

--- Analysis/analysisWithTwitter.py
+++ Analysis/analysisWithTwitter.py
@@ -9,13 +9,33 @@ from textblob import TextBlob
 import tweepy
 import re
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
 
 def cleanTweet(tweet):
     return ' '.join(re.sub("(@[A-Za-z0-9])|(#.*)|(\w+:\/\/\S+)", " ", tweet).split())
 
-#Auth Keys
-#Deleted Check the messanger
+def extractTweets(tweets):
+    return(list(tweet for tweet in tweets))
+    
+def analysisTweet(tweet):
+    res = TextBlob(tweet).sentences[0].sentiment.polarity
+    if(res > 0):    
+        return res,1
+    elif(res < 0):
+        return res,-1
+    return res,0
 
+def createDataFrame(tweetsList):
+    return pd.DataFrame({"Tweets":[i[2].text for i in tweetsList],
+                   "date":[i[2].created_at.date() for i in tweetsList],
+                   "analysis":[i[1] for i in tweetsList]
+                   })
+def plotSettings():
+    plt.yticks(np.arange(-1, 2, 1.0))
+#Auth Keys
+#Deleted Check the messanger fro security issues
 
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
@@ -28,25 +48,19 @@ tweets = tweepy.Cursor(api.search, q="#SALAH_Dont_Cry",lang="en").items(itemsCou
 
 
 #Extract Tweets
-listTweetsObj = [tweet for tweet in tweets]
-listTweetsTxt = [cleanTweet(tweet.text) for tweet in listTweetsObj]
+listTweetsObj = extractTweets(tweets)
+
 
 tweetsResult = []
+for i in listTweetsObj:
+    result = analysisTweet(cleanTweet(i.text))
+    tweetsResult.append((result[0],result[1],i))
 
-for i in listTweetsTxt:
-    blob = TextBlob(i)
-    res = blob.sentences[0].sentiment.polarity
-    resVal = 0
-    if(res > 0):    
-        resVal = 1
-    elif(res < 0):
-        resVal = -1
 
-    tweetsResult.append((res,resVal,i))
-    
+df = createDataFrame(tweetsResult)
 
-df = pd.DataFrame({"Tweets":listTweetsTxt,
-                   "date":[i.created_at for i in listTweetsObj],
-                   "analysis":[i[1] for i in tweetsResult]
-                   })
-print(df.head(5))
+plotSettings()
+plt.scatter(df['date'].tolist(),df['analysis'])
+plt.show()
+
+
